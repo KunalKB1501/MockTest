ba426bbb2b1080bfef22daa0b0a51948e4e6dba1
KunalKB1501 <130978186+KunalKB1501@users.noreply.github.com>
2023-04-29 15:47:14 +0530

    Update AnagramString.java

--- java/AnagramString.java
+++ java/AnagramString.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 public class AnagramString {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
+	//enter String
 	System.out.println("enter 1st string");
 	String str1=sc.nextLine();
 	System.out.println("enter 2nd string");
 	String str2=sc.nextLine();
+	//convert it into String array
 	char[] arr1=str1.toCharArray();
 	char[] arr2=str2.toCharArray();
-	
+	//Sort array
 	Arrays.sort(arr1);
 	Arrays.sort(arr2);
-	
+	//print them
 	System.out.println(arr1);
 	System.out.println(arr2);
+	//check whether String is anagram
 
 	if(Arrays.equals(arr1, arr2))
 	{
@@ -23,7 +26,6 @@ public static void main(String[] args) {
 	}
 	else
 	{
-		
 		System.out.println("given strings are not Anagram");
 	}
 }
